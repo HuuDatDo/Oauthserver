7edfb4101a082870e66b28f641c3f63a9dbef895
Shadow <dohuudat3110@gmail.com>
2022-03-24 11:12:58 +0700

    Create RefreshTokenRepository.java

---
+++ src/main/java/com/example/Oauthserver/Token/RefreshTokenRepository.java
@@ -0,0 +1,17 @@
+package com.example.Oauthserver.Token;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import com.example.Oauthserver.Token.RefreshToken;
+import com.example.Oauthserver.Oauth.User;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+  Optional<RefreshToken> findByToken(String token);
+
+  @Modifying
+  int deleteByUser(User user);
+}
