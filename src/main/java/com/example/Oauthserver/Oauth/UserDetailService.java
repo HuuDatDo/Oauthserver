ff62950579d4b2b7208eb8307bfe2969ed3001a4
Shadow <dohuudat3110@gmail.com>
2022-03-24 11:12:37 +0700

    Create UserDetailService.java

---
+++ src/main/java/com/example/Oauthserver/Oauth/UserDetailService.java
@@ -0,0 +1,22 @@
+package com.example.Oauthserver.Oauth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.example.Oauthserver.Oauth.User;
+import com.example.Oauthserver.Oauth.UserRepository;
+@Service
+public class UserDetailService implements UserDetailsService {
+	@Autowired(required = true)
+	UserRepository userRepository;
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+		return UserDetailsImpl.build(user);
+	}
+}
